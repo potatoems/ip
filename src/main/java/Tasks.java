96b257edaf705169a128077c03ee5e4c2fc1bd31
potatoems <5071spoh@gmail.com>
2025-01-31 02:37:08 +0800

    feat: Implement Task class to update and view task status and updateTask functions

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -1,18 +1,35 @@
 import java.util.ArrayList;
 
 public class Tasks {
-    private ArrayList<String> tasks;
+    final private ArrayList<Task> tasks;
 
     public Tasks() {
         this.tasks = new ArrayList<>();
     }
 
     public void add(String task) {
-        tasks.add(task);
+        Task t = new Task(task);
+        tasks.add(t);
         System.out.println("added: " + task);
     }
 
+    public void updateTask(int taskNumber, boolean toBeMarked) {
+        Task t = tasks.get(taskNumber - 1);
+
+        if (toBeMarked == !t.getStatus()) {
+            boolean isDone = t.updateStatus();
+            if (isDone) {
+                System.out.println("Nice! I've marked this task as done:\n" + t);
+            } else {
+                System.out.println("OK, I've marked this task as not done:\n" + t);
+            }
+        } else {
+            System.out.println("It seems like it's already been done");
+        }
+    }
+
     public void list() {
+        System.out.println("Here are the tasks in your list:");
         tasks.forEach(task -> System.out.println((tasks.indexOf(task) + 1) + ". " + task));
     }
 }
