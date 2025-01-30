c45eab82b1c167b5dcdeb9ec59b907e75e8401c0
potatoems <5071spoh@gmail.com>
2025-01-31 00:25:21 +0800

    feat: Rename and implement chatbot with greeting and farewell function

---
+++ src/main/java/Brownie.java
@@ -0,0 +1,9 @@
+public class Brownie{
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        String greeting = "\nHello! I'm BROWNIE\nWhat can I do for you?\n";
+        String farewell = "Bye. Hope to see you again soon!\n";
+        System.out.println(horizontalLine + greeting + horizontalLine);
+        System.out.println(farewell + horizontalLine);
+    }
+}
