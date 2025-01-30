ca82532a40c4d8a9e06bfe891cfc3718ae7c15b8
potatoems <5071spoh@gmail.com>
2025-01-31 00:43:29 +0800

    feat: Add Scanner to read command inputs and implement echo function

--- src/main/java/Brownie.java
+++ src/main/java/Brownie.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Brownie{
+    private static final String horizontalLine = "____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________";
-        String greeting = "\nHello! I'm BROWNIE\nWhat can I do for you?\n";
+        Scanner scanner = new Scanner(System.in);
+
+        String greeting = "Hello! I'm BROWNIE\nWhat can I do for you?\n";
         String farewell = "Bye. Hope to see you again soon!\n";
+
         System.out.println(horizontalLine + greeting + horizontalLine);
-        System.out.println(farewell + horizontalLine);
+
+        while(true) {
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println(farewell + horizontalLine);
+                break;
+            }
+
+            echo(command);
+        }
+    }
+
+    private static void echo(String command) {
+        System.out.println(horizontalLine + command + "\n" + horizontalLine);
     }
 }
